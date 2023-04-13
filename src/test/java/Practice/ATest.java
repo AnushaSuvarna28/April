b0cbfc2dab66d7b4c123dac773737a0f78ce76e4
AnushaSuvarna28 <122977109+AnushaSuvarna28@users.noreply.github.com>
2023-04-13 11:07:04 +0530

    Update ATest.java

--- src/test/java/Practice/ATest.java
+++ src/test/java/Practice/ATest.java
@@ -4,6 +4,6 @@ public class ATest
 {
 	public static void main(String[] args) {
 		System.out.println("This is A block");
-		System.out.println("This is updated A block");
+		System.out.println("This is updated A block")
 	}
 }
